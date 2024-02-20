10750bd287c71583b9543a541a1d995807c1b435
yusyus8016 <124086000+yusyus8016@users.noreply.github.com>
2024-02-20 17:50:41 -0500

    añadienedo al repo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [valor, setValor] = useState('');
+  const[resultados, setResultados] = useState([])
+
+  const buscarResultados =async () => {
+    const API_KEY = 'SQKbkCM0E01KfJIz77kiXA0zOTet7C1NN5y7ql8-F-g'
+    const URL = `https://api.unsplash.com/search/photos/?client_id=${API_KEY}&query=${valor}&per_page=30`;
+
+    const response = await fetch(URL);
+    const data = await response.json();
+    setResultados(data.results);
+    console.log(data);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='search__box'>
+        <input className='search__box--input' placeholder='buscar imagenes' onChange={e => setValor(e.target.value)}/>
+        <button onClick={() => buscarResultados()} className='search__box--btn'>Buscar</button>
+      </div>
+      <div className='main__content'>
+         <div className='main__content--grid'>
+        {
+        resultados.map((elemento, indice) => {
+          return(         
+              <img key={indice} src={elemento.urls.regular} alt=''/>            
+          )
+        })         
+        }
+        </div>
+      </div>
     </div>
   );
 }
